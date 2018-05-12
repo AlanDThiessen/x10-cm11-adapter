4aa541d5dec6a2e93bfbfa4d1fa89115cbdf386d
Alan Thiessen <AlanDThiessen@gmail.com>
2018-05-11 21:50:12 -0400

    Created package script.

---
+++ package.sh
@@ -0,0 +1,37 @@
+#/bin/bash
+
+# Thanks to the zigbee-addon https://github.com/mozilla-iot/zigbee-adapter
+# from Mozilla IoT  for parts of this script!
+
+# Architecture options:
+#    darwin-x64
+#    linux-arm
+#    linux-arm64
+#    linux-ia32
+#    linux-ia64
+#    win32-ia32
+#    win32-x64
+
+ADDON_ARCH=$1
+HOST_ARCH=`uname -s`
+
+if [ $HOST_ARCH == "Darwin" ]; then
+    SHASUM='shasum -a 256'
+else
+    SHASUM='shasum'
+fi
+
+if [ -n $ADDON_ARCH ]; then
+    rm -f SHA256SUMS
+    ${SHASUM} package.json *.js LICENSE > SHA256SUMS
+    find node_modules -type f -exec ${SHASUM} {} \; >> SHA256SUMS
+    TARFILE="$(npm pack)"
+    tar -xvf $TARFILE
+    TARFILE_ARCH="${TARFILE/.tgz/-${ADDON_ARCH}.tgz}"
+    cp -r node_modules ./package
+    tar -cvf ${TARFILE_ARCH} package
+    rm -rf package
+    rm -f $TARFILE
+    echo "Created ${TARFILE_ARCH}"
+fi
+
